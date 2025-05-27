927d2e6c7d72d116b54adbe047c0c7abfd4b9604
hade21 <abdurrohmanmuhammad123@gmail.com>
2025-05-27 11:25:34 +0800

    misc: installing shadcn ui, lottie-web, lordicon and modified hoem page

---
+++ src/components/animated-icon/mappin.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import { Player } from "@lordicon/react";
+import MapIcon from "../../../public/assets/icons/wired-lineal-18-location-pin-hover-jump.json";
+
+interface MapPinProps {
+  size?: number;
+}
+
+const MapPin = ({ size }: MapPinProps) => {
+  const playerRef = useRef<Player>(null);
+
+  useEffect(() => {
+    playerRef.current?.playFromBeginning();
+  }, []);
+  return (
+    <Player
+      ref={playerRef}
+      icon={MapIcon}
+      size={size}
+      onComplete={() => playerRef.current?.playFromBeginning}
+    />
+  );
+};
+
+export default MapPin;
