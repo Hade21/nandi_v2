bad97643954e1df1861909588874ac1230bdf0de
hade21 <abdurrohmanmuhammad123@gmail.com>
2025-08-06 16:01:51 +0800

    feat: add feature to update location using pin on maps

--- src/app/maps/maps-provider.tsx
+++ src/app/maps/maps-provider.tsx
@@ -1,8 +1,34 @@
 "use client";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { useUnitStore } from "@/utils/storeProvider";
+import { APIProvider, Map, MapMouseEvent } from "@vis.gl/react-google-maps";
 
 const MapsProvider = ({ children }: { children: React.ReactNode }) => {
   const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+  const updatingUnit = useUnitStore((state) => state.updatingUnit);
+  const selectedUnit = useUnitStore((state) => state.selectedUnit);
+  const setSelectedUnit = useUnitStore((state) => state.setSelectedUnit);
+
+  const pinOnMaps = (event: MapMouseEvent) => {
+    if (!updatingUnit && !selectedUnit.id) return;
+    if (event.detail.latLng) {
+      const location = {
+        lat: event.detail.latLng.lat,
+        lng: event.detail.latLng.lng,
+        alt: 0,
+        label: selectedUnit.name,
+      };
+      const updatedLocation = {
+        ...selectedUnit,
+        location: {
+          ...location,
+          lat: location.lat,
+          long: location.lng,
+          alt: location.alt,
+        },
+      };
+      setSelectedUnit(updatedLocation);
+    }
+  };
 
   if (!apiKey) {
     console.error(
@@ -19,6 +45,7 @@ const MapsProvider = ({ children }: { children: React.ReactNode }) => {
         defaultZoom={11}
         gestureHandling={"greedy"}
         disableDefaultUI={true}
+        onClick={updatingUnit ? pinOnMaps : undefined}
       >
         {children}
       </Map>
